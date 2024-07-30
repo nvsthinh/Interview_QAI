3871e61e9734a5d4df829311e24eabd9a3f179b9
nvsthinh <nguyenvansythinh.gv2003@gmail.com>
2024-07-30 14:26:31 +0700

    Update question 1

--- Question_1/main.py
+++ Question_1/main.py
@@ -1,1 +1,10 @@
 from model import AdaBoost
+from utils import load_data
+from sklearn.metrics import accuracy_score
+
+if __name__ == "__main__":
+    X_train, X_test, y_train, y_test = load_data()
+    model = AdaBoost(n_clf=10)
+    model.fit(X_train, y_train)
+    y_pred = model.predict(X_test)
+    print(f"Accuracy: {accuracy_score(y_test, y_pred)}")
